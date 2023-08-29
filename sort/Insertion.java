1bbf0a746eb5139d005b2761c83a1bbd30bac4d5
Maxwell Castillo <maxwell.castillo@mrjaffesclass.com>
2023-08-29 12:40:33 -0700

    Selection and insertion

--- sort/Insertion.java
+++ sort/Insertion.java
@@ -10,33 +10,44 @@ import java.util.Arrays;
  */
 public class Insertion
 {
-  
-  /**
-   * Returns the sorted array
-   */
-  public static int[] sort(int[] arr) {
-    // Your algorithm goes here!
 
-  }
-  
-  public static void main(String[] args) {
-    int[] arr = {53,85,93,25,39,27,42,5,24,45,33,51,5,80,4,7,91,
-      31,66,71,32,19,79,58,61,82,89,63,7,4,50,10,48,24,75,19,22,
-      73,54,51,25,33,20,52,79,97,70,54,63,49};    
-    
-    // Test the sort
-    testSort(sort(arr));
-  }
-  
-  public static void testSort(int[] arr) {
-    for (int i=0; i<arr.length-1; i++) {
-      if (arr[i] > arr[i+1]) {
-        System.out.println("FAIL at index "+i);
-        System.out.println(Arrays.toString(arr));
-        return;
-      }
+    /**
+     * Returns the sorted array
+     */
+    public static int[] sort(int[] arr) {
+        int temp;
+        for (int i = 1; i< arr.length; i++){
+            if (arr[i] < arr[i-1]){
+                int j = i;
+                while (arr[j] < arr[j-1]){
+                    temp = arr[j-1];
+                    arr[i-1] = arr[j];
+                    arr[j] = temp;
+                    j--;
+                }
+            }
+        }
+        return arr;
     }
-    System.out.println("SUCCESS!");
-  }
+
+    public static void main(String[] args) {
+        int[] arr = {53,85,93,25,39,27,42,5,24,45,33,51,5,80,4,7,91,
+                31,66,71,32,19,79,58,61,82,89,63,7,4,50,10,48,24,75,19,22,
+                73,54,51,25,33,20,52,79,97,70,54,63,49};    
+
+        // Test the sort
+        testSort(sort(arr));
+    }
+
+    public static void testSort(int[] arr) {
+        for (int i=0; i<arr.length-1; i++) {
+            if (arr[i] > arr[i+1]) {
+                System.out.println("FAIL at index "+i);
+                System.out.println(Arrays.toString(arr));
+                return;
+            }
+        }
+        System.out.println("SUCCESS!");
+    }
 
 }
--- sort/Selection.java
+++ sort/Selection.java
@@ -10,33 +10,48 @@ import java.util.Arrays;
  */
 public class Selection
 {
-  
-  /**
-   * Returns the sorted array
-   */
-  public static int[] sort(int[] arr) {
-    // Your algorithm goes here!
 
-  }
-  
-  public static void main(String[] args) {
-    int[] arr = {53,85,93,25,39,27,42,5,24,45,33,51,5,80,4,7,91,
-      31,66,71,32,19,79,58,61,82,89,63,7,4,50,10,48,24,75,19,22,
-      73,54,51,25,33,20,52,79,97,70,54,63,49};    
-    
-    // Test the sort
-    testSort(sort(arr));
-  }
-  
-  public static void testSort(int[] arr) {
-    for (int i=0; i<arr.length-1; i++) {
-      if (arr[i] > arr[i+1]) {
-        System.out.println("FAIL at index "+i);
-        System.out.println(Arrays.toString(arr));
-        return;
-      }
+    /**
+     * Returns the sorted array
+     */
+    public static int[] sort(int[] arr) {
+        int least = arr[0];
+        int temp;
+        int pos = 0;
+        for (int i = 0; i < arr.length; i++){
+            least = arr[i];
+            for (int j = i; j < arr.length; j++){
+                
+                if (arr[j] < least){
+                    least = arr[j];
+                    pos = j;
+                }
+            }
+            temp = least;
+            arr[i] = arr[pos];
+            arr[pos] = temp;
+        }
+        return arr;
     }
-    System.out.println("SUCCESS!");
-  }
+
+    public static void main(String[] args) {
+        int[] arr = {53,85,93,25,39,27,42,5,24,45,33,51,5,80,4,7,91,
+                31,66,71,32,19,79,58,61,82,89,63,7,4,50,10,48,24,75,19,22,
+                73,54,51,25,33,20,52,79,97,70,54,63,49};    
+
+        // Test the sort
+        testSort(sort(arr));
+    }
+
+    public static void testSort(int[] arr) {
+        for (int i=0; i<arr.length-1; i++) {
+            if (arr[i] > arr[i+1]) {
+                System.out.println("FAIL at index "+i);
+                System.out.println(Arrays.toString(arr));
+                return;
+            }
+        }
+        System.out.println("SUCCESS!");
+    }
 
 }
