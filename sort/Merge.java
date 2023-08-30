306718d80824049572b3bb02779148d5a4043eb5
Maxwell Castillo <maxwell.castillo@mrjaffesclass.com>
2023-08-30 12:30:07 -0700

    mergeSort

--- sort/Merge.java
+++ sort/Merge.java
@@ -10,33 +10,64 @@ import java.util.Arrays;
  */
 public class Merge
 {
-  
-  /**
-   * Returns the sorted array
-   */
-  public static int[] sort(int[] arr) {
-    // Your algorithm goes here!
 
-  }
-  
-  public static void main(String[] args) {
-    int[] arr = {53,85,93,25,39,27,42,5,24,45,33,51,5,80,4,7,91,
-      31,66,71,32,19,79,58,61,82,89,63,7,4,50,10,48,24,75,19,22,
-      73,54,51,25,33,20,52,79,97,70,54,63,49};    
-    
-    // Test the sort
-    testSort(sort(arr));
-  }
-  
-  public static void testSort(int[] arr) {
-    for (int i=0; i<arr.length-1; i++) {
-      if (arr[i] > arr[i+1]) {
-        System.out.println("FAIL at index "+i);
-        System.out.println(Arrays.toString(arr));
-        return;
-      }
+    /**
+     * Returns the sorted array
+     */
+    public static int[] mergeSort(int[] arr, int n) {
+        if (n<2){
+            return arr;
+        }
+        int mid = n/2;
+        int[] l = new int[mid];
+        int[] r = new int[n-mid];
+
+        for (int i = 0; i < mid; i++){
+            l[i] = arr[i];
+        }
+        mergeSort(l,mid);
+        mergeSort(r, n-mid);
+
+        merge(arr, l, r, mid, n-mid);
+        return arr;
     }
-    System.out.println("SUCCESS!");
-  }
+
+    public static void merge(int[] a, int[] l,int[] r, int left, int right){
+        int i = 0, j = 0, k = 0;
+        while (i < left && j < right) {
+            if (l[i] <= r[j]) {
+                a[k++] = l[i++];
+            }
+            else {
+                a[k++] = r[j++];
+            }
+        }
+        while (i < left) {
+            a[k++] = l[i++];
+        }
+        while (j < right) {
+            a[k++] = r[j++];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {53,85,93,25,39,27,42,5,24,45,33,51,5,80,4,7,91,
+                31,66,71,32,19,79,58,61,82,89,63,7,4,50,10,48,24,75,19,22,
+                73,54,51,25,33,20,52,79,97,70,54,63,49};    
+        int n = arr.length;
+        // Test the sort
+        testSort(mergeSort(arr, n));
+    }
+
+    public static void testSort(int[] arr) {
+        for (int i=0; i<arr.length-1; i++) {
+            if (arr[i] > arr[i+1]) {
+                System.out.println("FAIL at index "+i);
+                System.out.println(Arrays.toString(arr));
+                return;
+            }
+        }
+        System.out.println("SUCCESS!");
+    }
 
 }
